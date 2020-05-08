f099d26fa17130090e28a13f063069441df5b738
Juraj Veverka <juraj.veverka@inter-net.sk>
2020-05-08 09:35:35 +0200

    fixed buf in configuration file load/save cycle

--- iam-persistence/iam-persistence-file-system/src/main/java/itx/iamservice/persistence/filesystem/FileSystemDataLoadServiceImpl.java
+++ iam-persistence/iam-persistence-file-system/src/main/java/itx/iamservice/persistence/filesystem/FileSystemDataLoadServiceImpl.java
@@ -7,16 +7,21 @@ import itx.iamservice.core.services.impl.caches.ModelCacheImpl;
 import itx.iamservice.core.services.persistence.DataLoadService;
 import itx.iamservice.core.services.persistence.wrappers.ModelWrapper;
 import itx.iamservice.core.services.persistence.PersistenceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class FileSystemDataLoadServiceImpl implements DataLoadService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FileSystemDataLoadServiceImpl.class);
+
     private final ModelWrapper modelWrapper;
     private final PersistenceService persistenceService;
 
     public FileSystemDataLoadServiceImpl(Path dataFile, PersistenceService persistenceService) throws IOException {
+        LOG.info("FileSystemPersistence: loading from dataFile={}", dataFile);
         ObjectMapper mapper = new ObjectMapper();
         this.modelWrapper = mapper.readValue(dataFile.toFile(), ModelWrapper.class);
         this.persistenceService = persistenceService;

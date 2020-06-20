795509e27f945bf61bdcdd54955788408235c360
Juraj Veverka <gergej123@gmail.com>
2020-06-20 19:44:46 +0200

    File system persistence performance test

--- iam-persistence/iam-persistence-file-system/src/test/java/itx/iamservice/persistence/filesystem/tests/FileSystemPersistenceTests.java
+++ iam-persistence/iam-persistence-file-system/src/test/java/itx/iamservice/persistence/filesystem/tests/FileSystemPersistenceTests.java
@@ -12,15 +12,20 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.Security;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FileSystemPersistenceTests {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FileSystemPersistenceTests.class);
 
     @TempDir
     static Path sharedTempDir;
@@ -49,6 +54,24 @@ public class FileSystemPersistenceTests {
 
         DataLoadService dataLoadService = new FileSystemDataLoadServiceImpl(serializedModel, persistenceService);
         ModelCache modelCache = dataLoadService.populateCache();
+        assertNotNull(modelCache);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+        Path dataFilePath = Paths.get("/tmp/iam-data.json");
+        FileSystemPersistenceServiceImpl persistenceService = new FileSystemPersistenceServiceImpl(dataFilePath, true);
+        long timeStamp = System.nanoTime();
+        ModelCache modelCache = ModelUtils.createModel(3, 3, 4, 100, 5, 3, persistenceService);
+        LOG.info("model create time: {} ms", ((System.nanoTime() - timeStamp)/1_000_000F));
+        timeStamp = System.nanoTime();
+        persistenceService.flush();
+        LOG.info("model flush time: {} ms", ((System.nanoTime() - timeStamp)/1_000_000F));
+        assertNotNull(modelCache);
+        DataLoadService dataLoadService = new FileSystemDataLoadServiceImpl(dataFilePath, persistenceService);
+        timeStamp = System.nanoTime();
+        modelCache = dataLoadService.populateCache();
+        LOG.info("model load time: {} ms", ((System.nanoTime() - timeStamp)/1_000_000F));
         assertNotNull(modelCache);
     }
 

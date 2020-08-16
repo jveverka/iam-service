cc2aff54d203f6da41a5993fb6d8007f91c2389f
Juraj Veverka <gergej123@gmail.com>
2020-08-17 00:05:10 +0200

    fixed consent screen handling

--- iam-service/src/main/resources/static/login-form.js
+++ iam-service/src/main/resources/static/login-form.js
@@ -3,6 +3,7 @@ var login_request = null;
 var authorization_code_response = null;
 var scopes_selected = [];
 var tokens = null;
+var scope_input_elements = [];
 
 function splitScopesToArray(scope_string) {
     scope_string = scope_string.trim();
@@ -72,28 +73,54 @@ function onLoginOk() {
     console.log("on-login-ok ...");
     available_scopes = authorization_code_response.availableScopes.values;
     selector_element = document.getElementById("scopes-selector");
-        for (i = 0; i < available_scopes.length; i++) {
-            console.log("populating selector:  " + available_scopes[i]);
-            var div_wrapper = document.createElement("div");
-            div_wrapper.setAttribute("class", "form-check");
-            var input_element = document.createElement("input");
-            input_element.setAttribute("class", "form-check-input");
-            input_element.setAttribute("type", "checkbox");
-            input_element.setAttribute("value", available_scopes[i]);
-            input_element.setAttribute("id", "scope-" + i);
-            input_element.setAttribute("name", "scope-" + i);
-            input_element.setAttribute("onclick", "onScopeSelect(this);");
-            var label_element = document.createElement("label");
-            label_element.setAttribute("for", "scope-" + i);
-            label_element.setAttribute("class", "form-check-label");
-            var text_node = document.createTextNode(available_scopes[i]);
-            label_element.appendChild(text_node);
-            var br_element = document.createElement("br");
-            div_wrapper.appendChild(input_element);
-            div_wrapper.appendChild(label_element);
-            selector_element.appendChild(div_wrapper);
-            selector_element.appendChild(br_element);
-        }
+    for (i = 0; i < available_scopes.length; i++) {
+        console.log("populating selector:  " + available_scopes[i]);
+        var div_wrapper = document.createElement("div");
+        div_wrapper.setAttribute("class", "form-check");
+        var input_element = document.createElement("input");
+        input_element.setAttribute("class", "form-check-input");
+        input_element.setAttribute("type", "checkbox");
+        input_element.setAttribute("value", available_scopes[i]);
+        input_element.setAttribute("id", "scope-" + i);
+        input_element.setAttribute("name", "scope-" + i);
+        input_element.setAttribute("onclick", "onScopeSelect(this);");
+        var label_element = document.createElement("label");
+        label_element.setAttribute("for", "scope-" + i);
+        label_element.setAttribute("class", "form-check-label");
+        var text_node = document.createTextNode(available_scopes[i]);
+        label_element.appendChild(text_node);
+        var br_element = document.createElement("br");
+        div_wrapper.appendChild(input_element);
+        div_wrapper.appendChild(label_element);
+        selector_element.appendChild(div_wrapper);
+        selector_element.appendChild(br_element);
+        scope_input_elements[i] = input_element;
+    }
+
+    if (available_scopes.length > 0) {
+        console.log("populating selector:  adding select all option");
+        selector_element = document.getElementById("scopes-selector");
+        var div_wrapper = document.createElement("div");
+        div_wrapper.setAttribute("class", "form-group form-check");
+        var input_element = document.createElement("input");
+        input_element.setAttribute("class", "form-check-input");
+        input_element.setAttribute("type", "checkbox");
+        input_element.setAttribute("id", "scope-select-all");
+        input_element.setAttribute("name", "scope-select-all");
+        input_element.setAttribute("onclick", "onAllScopesSelect();");
+        var label_element = document.createElement("label");
+        label_element.setAttribute("for", "scope-select-all");
+        label_element.setAttribute("class", "form-check-label");
+        var text_node = document.createTextNode("select all");
+        label_element.appendChild(text_node);
+        var br_element = document.createElement("br");
+        div_wrapper.appendChild(input_element);
+        div_wrapper.appendChild(label_element);
+        selector_element.appendChild(div_wrapper);
+        selector_element.appendChild(br_element);
+    } else {
+        console.log("no scopes available");
+    }
 
     hideElement(document.getElementById("login-container"));
     showElement(document.getElementById("consent-container"));
@@ -125,30 +152,15 @@ function getBaseUrl() {
 
 function onScopeSelect(input_element) {
    console.log("on scope selected: " + input_element.name + " " + input_element.checked);
-   if (input_element.checked) {
-       scopes_selected.push(input_element.value);
-   } else {
-       for (i = 0; i < scopes_selected.length; i++) {
-           if (input_element.value == scopes_selected[i])  {
-               delete scopes_selected[i];
-           }
-       }
+   scopes_selected = [];
+   for (i = 0; i < scope_input_elements.length; i++) {
+     if (scope_input_elements[i].checked) {
+         scopes_selected.push(scope_input_elements[i].value);
+     }
    }
-   onScopeChange();
-};
-
-function onScopeChange() {
-   var result_scopes = "";
-   for (i = 0; i < scopes_selected.length; i++) {
-       if (!(!scopes_selected[i] || 0 === scopes_selected[i].length)) {
-          if (result_scopes === "") {
-            result_scopes = scopes_selected[i];
-          } else {
-            result_scopes = result_scopes + " " + scopes_selected[i];
-          }
-       }
-   }
-   console.log("result scopes: " + result_scopes);
+   scope_select_all_element = document.getElementById("scope-select-all");
+   scope_select_all_element.checked = false;
+   console.log("result scopes: " + scopes_selected);
 };
 
 function onConsentOk() {
@@ -159,3 +171,18 @@ function onConsentOk() {
 function onConsentFailed() {
    console.log("on Consent Failed ...");
 };
+
+function onAllScopesSelect() {
+   console.log("on all scopes selected");
+   scope_select_all_element = document.getElementById("scope-select-all");
+   for (i = 0; i < scope_input_elements.length; i++) {
+       scope_input_elements[i].checked = scope_select_all_element.checked;
+   }
+   if (scope_select_all_element.checked) {
+       scopes_selected = authorization_code_response.availableScopes.values;
+   } else {
+       scopes_selected = [];
+   }
+   console.log("result scopes: " + scopes_selected);
+};
+

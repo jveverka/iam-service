8fae5c8a52bbf1194b28e80ad208d0c8a3425249
Juraj Veverka <gergej123@gmail.com>
2020-10-30 20:10:14 +0100

    junit test cleanup

--- iam-examples/spring-method-security/src/test/java/itx/iamservice/examples/methodsecurity/ittests/MethodSecurityTestsIT.java
+++ iam-examples/spring-method-security/src/test/java/itx/iamservice/examples/methodsecurity/ittests/MethodSecurityTestsIT.java
@@ -1,19 +1,19 @@
 package itx.iamservice.examples.methodsecurity.ittests;
 
 
-import itx.iamservice.core.dto.CreateOrganization;
-import itx.iamservice.core.dto.CreateProject;
-import itx.iamservice.core.model.ClientCredentials;
 import itx.iamservice.core.model.ClientId;
 import itx.iamservice.core.model.OrganizationId;
 import itx.iamservice.core.model.ProjectId;
 import itx.iamservice.core.model.UserId;
-import itx.iamservice.core.services.dto.CreateClientRequest;
-import itx.iamservice.core.services.dto.CreatePermissionRequest;
-import itx.iamservice.core.services.dto.CreateUserRequest;
-import itx.iamservice.core.services.dto.SetUserNamePasswordCredentialsRequest;
+import itx.iamservice.core.model.utils.ModelUtils;
+import itx.iamservice.core.services.dto.SetupOrganizationRequest;
+import itx.iamservice.core.services.dto.SetupOrganizationResponse;
 import itx.iamservice.core.services.dto.TokenResponse;
 import itx.iamservice.examples.methodsecurity.dto.SystemInfo;
+import itx.iamservice.serviceclient.IAMAuthorizerClient;
+import itx.iamservice.serviceclient.IAMServiceClientBuilder;
+import itx.iamservice.serviceclient.IAMServiceManagerClient;
+import itx.iamservice.serviceclient.impl.AuthenticationException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -23,16 +23,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.createClientOnTheProject;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.createNewOrganization;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.createPermissionOnProject;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.createProject;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.createUserOnProject;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.getTokenResponseForIAMAdmins;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.getTokenResponseForUserNameAndPassword;
-import static itx.iamservice.client.spring.httpclient.HttpClientTestUtils.setUsernamePasswordCredentialsForProjectAndUser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -48,12 +43,24 @@ public class MethodSecurityTestsIT {
     private static OrganizationId organizationId;
     private static ProjectId projectId;
     private static UserId userId;
+    private static ClientId clientId;
+
+    private static IAMServiceManagerClient iamServiceManagerClient;
 
     @BeforeAll
-    public static void init() {
+    public static void init() throws MalformedURLException {
         restTemplate = new TestRestTemplate();
         iamServerPort = 8080;
         resourceServerPort = 8082;
+        organizationId = OrganizationId.from("it-testing-001");
+        projectId = ProjectId.from("spring-method-security");
+        userId = UserId.from("user-001");
+        clientId = ClientId.from("client-001");
+        URL baseUrl = new URL("http://localhost:" + iamServerPort);
+        iamServiceManagerClient = IAMServiceClientBuilder.builder()
+                .withBaseUrl(baseUrl)
+                .withConnectionTimeout(60L, TimeUnit.SECONDS)
+                .build();
     }
 
     @Test
@@ -75,27 +82,23 @@ public class MethodSecurityTestsIT {
 
     @Test
     @Order(3)
-    public void getAdminAccessTokens() {
-        adminTokens = getTokenResponseForIAMAdmins(restTemplate, iamServerPort);
+    public void getAdminAccessTokens() throws AuthenticationException {
+        adminTokens = iamServiceManagerClient
+                .getIAMAdminAuthorizerClient()
+                .getAccessTokensOAuth2UsernamePassword("admin", "secret", ModelUtils.IAM_ADMIN_CLIENT_ID, "top-secret");
         assertNotNull(adminTokens);
     }
 
     @Test
     @Order(4)
-    public void createOrganizationProjectAndUsers() {
-        CreateOrganization request = new CreateOrganization("it-testing-001", "IT Testing");
-        organizationId = createNewOrganization(adminTokens.getAccessToken(), restTemplate, iamServerPort, request);
-        CreateProject createProjectRequest = new CreateProject("spring-method-security", "Method Security Project", Set.of("methodsecurity"));
-        projectId = createProject(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, createProjectRequest);
-        createPermissionOnProject(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, projectId, new CreatePermissionRequest("methodsecurity", "data", "read"));
-        createPermissionOnProject(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, projectId, new CreatePermissionRequest("methodsecurity", "data", "modify"));
-        CreateUserRequest createUserRequest = new CreateUserRequest(UserId.from("user-001"), "name",  3600*1000L, 3600*1000L, "admin@email.com");
-        userId = createUserOnProject(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, projectId, createUserRequest);
-        SetUserNamePasswordCredentialsRequest setUserNamePasswordCredentialsRequest = new SetUserNamePasswordCredentialsRequest(userId.getId(), "top-secret");
-        setUsernamePasswordCredentialsForProjectAndUser(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, projectId, userId, setUserNamePasswordCredentialsRequest);
-        CreateClientRequest createClientRequest = new CreateClientRequest(ClientId.from("client-001"), "name", 3600*1000L, 3600*1000L, "top-secret");
-        ClientCredentials clientCredentials = createClientOnTheProject(adminTokens.getAccessToken(), restTemplate, iamServerPort, organizationId, projectId, createClientRequest);
-        userTokens = getTokenResponseForUserNameAndPassword(restTemplate, iamServerPort, userId.getId(), "top-secret", clientCredentials.getId(), clientCredentials.getSecret(), organizationId, projectId);
+    public void createOrganizationProjectAndUsers() throws AuthenticationException {
+        SetupOrganizationRequest setupOrganizationRequest = new SetupOrganizationRequest(organizationId.getId(), "IT Testing",
+                projectId.getId(),  "Method Security Project",
+                clientId.getId(), "top-secret", userId.getId(),  "top-secret", "admin@email.com", Set.of("methodsecurity"));
+        SetupOrganizationResponse setupOrganizationResponse = iamServiceManagerClient.setupOrganization(adminTokens.getAccessToken(), setupOrganizationRequest);
+        assertNotNull(setupOrganizationResponse);
+        IAMAuthorizerClient iamAuthorizerClient = iamServiceManagerClient.getIAMAuthorizerClient(organizationId, projectId);
+        userTokens = iamAuthorizerClient.getAccessTokensOAuth2UsernamePassword(userId.getId(), "top-secret", clientId, "top-secret");
         assertNotNull(userTokens);
     }
 

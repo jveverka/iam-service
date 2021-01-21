6b6a89b21f54a6bcf3ad600e93ee69224b743313
Juraj Veverka <gergej123@gmail.com>
2021-01-21 21:21:41 +0100

    json deserialization fixed

--- iam-common/iam-model/src/main/java/one/microproject/iamservice/core/model/keys/Id.java
+++ iam-common/iam-model/src/main/java/one/microproject/iamservice/core/model/keys/Id.java
@@ -2,20 +2,39 @@ package one.microproject.iamservice.core.model.keys;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import one.microproject.iamservice.core.model.ClientId;
+import one.microproject.iamservice.core.model.KeyId;
+import one.microproject.iamservice.core.model.ModelId;
+import one.microproject.iamservice.core.model.OrganizationId;
+import one.microproject.iamservice.core.model.PermissionId;
+import one.microproject.iamservice.core.model.ProjectId;
+import one.microproject.iamservice.core.model.RoleId;
+import one.microproject.iamservice.core.model.UserId;
 
 import java.util.Objects;
 
 @JsonTypeInfo(
-        use = JsonTypeInfo.Id.CLASS,
-        include = JsonTypeInfo.As.PROPERTY,
-        property = "type")
+        use = JsonTypeInfo.Id.NAME,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ClientId.class, name = "ClientId"),
+        @JsonSubTypes.Type(value = KeyId.class, name = "KeyId"),
+        @JsonSubTypes.Type(value = OrganizationId.class, name = "OrganizationId"),
+        @JsonSubTypes.Type(value = ProjectId.class, name = "ProjectId"),
+        @JsonSubTypes.Type(value = PermissionId.class, name = "PermissionId"),
+        @JsonSubTypes.Type(value = RoleId.class, name = "RoleId"),
+        @JsonSubTypes.Type(value = UserId.class, name = "UserId"),
+        @JsonSubTypes.Type(value = ModelId.class, name = "ModelId")
+})
 public abstract class Id {
 
     private final String id;
 
     @JsonCreator
-    public Id(@JsonProperty("id") String id) {
+    protected Id(@JsonProperty("id") String id) {
         this.id = id;
     }
 

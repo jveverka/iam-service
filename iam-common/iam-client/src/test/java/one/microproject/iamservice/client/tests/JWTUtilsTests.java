cdf157d375ce4f20a91a6dd85ee260065f94c068
Juraj Veverka <gergej123@gmail.com>
2021-03-26 23:13:15 +0100

    junits improved

--- iam-common/iam-client/src/test/java/one/microproject/iamservice/client/tests/JWTUtilsTests.java
+++ iam-common/iam-client/src/test/java/one/microproject/iamservice/client/tests/JWTUtilsTests.java
@@ -1,8 +1,15 @@
 package one.microproject.iamservice.client.tests;
 
 import one.microproject.iamservice.client.JWTUtils;
+import one.microproject.iamservice.core.dto.StandardTokenClaims;
 import one.microproject.iamservice.core.model.JWToken;
+import one.microproject.iamservice.core.model.OrganizationId;
+import one.microproject.iamservice.core.model.ProjectId;
+import one.microproject.iamservice.core.model.TokenType;
 import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -38,6 +45,14 @@ class JWTUtilsTests {
     void testCreateAuthorizationHeader() {
         String token = JWTUtils.createAuthorizationHeader("token");
         assertEquals("Bearer token", token);
+    }
+
+    @Test
+    void testValidatePermissions() throws URISyntaxException {
+        StandardTokenClaims standardTokenClaims = new StandardTokenClaims("",  "", "", Set.of(), Set.of(),
+                OrganizationId.from(""), ProjectId.from(""), TokenType.BEARER);
+        boolean result = JWTUtils.validatePermissions(standardTokenClaims, Set.of(), Set.of());
+        assertEquals(true, result);
     }
 
 }

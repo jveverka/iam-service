f0041ecc04c67acc06308f841a4660552d2d2cc5
Juraj Veverka <gergej123@gmail.com>
2021-04-02 13:03:43 +0200

    unit tests improved

--- iam-common/iam-client/src/test/java/one/microproject/iamservice/client/tests/JWTUtilsTests.java
+++ iam-common/iam-client/src/test/java/one/microproject/iamservice/client/tests/JWTUtilsTests.java
@@ -4,12 +4,18 @@ import one.microproject.iamservice.client.JWTUtils;
 import one.microproject.iamservice.core.dto.StandardTokenClaims;
 import one.microproject.iamservice.core.model.JWToken;
 import one.microproject.iamservice.core.model.OrganizationId;
+import one.microproject.iamservice.core.model.Permission;
+import one.microproject.iamservice.core.model.PermissionParsingException;
 import one.microproject.iamservice.core.model.ProjectId;
 import one.microproject.iamservice.core.model.TokenType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.net.URISyntaxException;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -47,12 +53,27 @@ class JWTUtilsTests {
         assertEquals("Bearer token", token);
     }
 
-    @Test
-    void testValidatePermissions() throws URISyntaxException {
-        StandardTokenClaims standardTokenClaims = new StandardTokenClaims("",  "", "", Set.of(), Set.of(),
-                OrganizationId.from(""), ProjectId.from(""), TokenType.BEARER);
-        boolean result = JWTUtils.validatePermissions(standardTokenClaims, Set.of(), Set.of());
-        assertEquals(true, result);
+    private static Stream<Arguments> provideValidatePermissionsData() throws URISyntaxException, PermissionParsingException {
+        return Stream.of(
+                Arguments.of(createStandardTokenClaims(Set.of()), Set.of(), Set.of(), Boolean.TRUE),
+                Arguments.of(createStandardTokenClaims(Set.of("s1.r1.a1", "s2.a2.r2")), Set.of(), Set.of(), Boolean.TRUE),
+                Arguments.of(createStandardTokenClaims(Set.of("s1.r1.a1")), Set.of(), Set.of(Permission.from("s1.r1.a1")), Boolean.TRUE),
+                Arguments.of(createStandardTokenClaims(Set.of("s2.r2.a2")), Set.of(), Set.of(Permission.from("s1.r1.a1")), Boolean.FALSE),
+                Arguments.of(createStandardTokenClaims(Set.of("s1.r1.a1")), Set.of(Permission.from("s1.r1.a1")), Set.of(), Boolean.TRUE),
+                Arguments.of(createStandardTokenClaims(Set.of("s2.r2.a2")), Set.of(Permission.from("s1.r1.a1")), Set.of(), Boolean.FALSE)
+        );
+    }
+
+    private static StandardTokenClaims createStandardTokenClaims(Set<String> scope) throws URISyntaxException {
+        return new StandardTokenClaims("k001",  "iss", "sub", Set.of(), scope,
+                OrganizationId.from("o-001"), ProjectId.from("p-001"), TokenType.BEARER);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidatePermissionsData")
+    void testValidatePermissions(StandardTokenClaims standardTokenClaims, Set<Permission> requiredAdminPermissions, Set<Permission> requiredApplicationPermissions, Boolean expectedResult) {
+        boolean result = JWTUtils.validatePermissions(standardTokenClaims, requiredAdminPermissions, requiredApplicationPermissions);
+        assertEquals(expectedResult, result);
     }
 
 }
